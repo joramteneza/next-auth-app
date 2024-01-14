703230f892142b5267ef2ce3593ab194da456abb
Joram <joram.teneza@threetwentyfive.com.au>
2024-01-14 11:47:39 +0800

    Changes

---
+++ app/(models)/User.ts
@@ -0,0 +1,26 @@
+import mongoose, { Schema, Document, Model } from "mongoose";
+
+mongoose.connect(process.env.MONGODB_URI ?? "");
+mongoose.Promise = global.Promise;
+
+interface IUser extends Document {
+  name: string;
+  email: string;
+  password: string;
+}
+
+const userSchema = new Schema<IUser>(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User: Model<IUser> =
+  mongoose.models.User || mongoose.model<IUser>("User", userSchema);
+
+export default User;
---
+++ app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,76 @@
+import { NextAuthOptions, User } from "next-auth";
+import { AdapterUser } from "next-auth/adapters";
+import { JWT } from "next-auth/jwt";
+import GitHubProvider, { GithubProfile } from "next-auth/providers/github";
+import GoogleProvider, { GoogleProfile } from "next-auth/providers/google";
+import CredentialsProvider, {
+  CredentialsProviderType,
+} from "next-auth/providers/credentials";
+
+export const options: NextAuthOptions = {
+  providers: [
+    GitHubProvider({
+      profile(profile: any) {
+        console.log("GitHub Profile: ", profile);
+
+        let userRole = "GitHub User";
+        if (profile?.email === "joram.teneza@gmail.com") {
+          userRole = "admin";
+        }
+
+        return {
+          ...profile,
+          role: userRole,
+        };
+      },
+      clientId: process.env.GITHUB_ID ?? "",
+      clientSecret: process.env.GITHUB_SECRET ?? "",
+    }),
+    GoogleProvider({
+      profile(profile: GoogleProfile) {
+        console.log("Google Profile: ", profile);
+
+        let userRole = "Google User"; // Define userRole for Google users
+
+        return {
+          ...profile,
+          id: profile.sub,
+          role: userRole,
+        };
+      },
+      clientId: process.env.GOOGLE_ID ?? "",
+      clientSecret: process.env.GOOGLE_SECRET ?? "",
+    }),
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: {
+          label: "email",
+          type: "text",
+          placeholder: "your-email",
+        },
+        password: {
+          label: "password",
+          type: "password",
+          placeholder: "your-password",
+        },
+      },
+      async authorize(credentials: any) {
+        try {
+        } catch (error) {
+          return null;
+        }
+      },
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) token.role = user.role;
+      return token;
+    },
+    async session({ session, token }) {
+      if (session?.user) session.user.role = token.role;
+      return session;
+    },
+  },
+};
